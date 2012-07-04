6abb9e1f838a097207ce005f6c84613b63c26b27
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-07-04 04:11:50 -0400

    Add some sane defaults for settings.

---
+++ config/initializers/setting_defaults.rb
@@ -0,0 +1,6 @@
+# The default names (primarily used by the unit tests)
+Settings.defaults[:instance_name] = 'Spark'
+Settings.defaults[:org_name] = 'Your Name Here'
+
+# Whether or not we want database auth.
+Settings.defaults[:db_auth] = true
