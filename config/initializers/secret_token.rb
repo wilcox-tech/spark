56097e4b0ea3fa7ad5e194381ce1616d3634d4fb
Andrew Wilcox <AWilcox@Wilcox-Tech.com>
2012-07-04 03:49:28 -0400

    Initialised new, empty Rails project with RSpec tests.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spark::Application.config.secret_token = 'faa3d7795067737d912518b3056ebff859d10532ea40f08d7b38d54f66a858c5fb016646d3a84df4aaea6576bb0727bb9e50d81259eb513e10cf0431cb7b48c9'
